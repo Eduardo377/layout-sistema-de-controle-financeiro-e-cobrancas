f3c9f737fb0b0ee1d46e31b0bae4a546e442ed16
Lucas Kennedy <lukakfp@gmail.com>
2021-11-22 20:54:24 -0300

    componente para controle de rotas privadas

--- src/rotas/RotaPrivada.jsx
+++ src/rotas/RotaPrivada.jsx
@@ -1,7 +1,9 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const RotaPrivada = () => {
-  console.log("Essa é uma rota privada");
+  const { pathname } = useLocation();
+  console.log("Essa é uma rota privada => " + pathname);
+
   // regras aqui
 
   return <Outlet />;
