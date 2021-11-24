905b96e72158c1aca3cc54123efbf6312ee27194
higor-d3v <hgrprofissional@gmail.com>
2021-11-24 15:54:45 -0300

    redirecionamento

--- src/paginas/cadastro/index.jsx
+++ src/paginas/cadastro/index.jsx
@@ -1,6 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 const Cadastro = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  useEffect(() => {
+    pathname === "/cadastro" && navigate("inicio");
+  }, []);
+
   return (
     <div>
       <h1>Página de Cadastro</h1>
