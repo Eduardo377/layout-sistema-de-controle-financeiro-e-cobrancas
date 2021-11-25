2016ec043f722ee03d1bba4cb775cceb0ecceaf6
Lucas Kennedy <lukakfp@gmail.com>
2021-11-25 10:45:50 -0300

    Hml (#10)
    * package.json
    
    * limpar public
    
    * yarn
    
    * importei os estilos globais
    
    * icones
    
    * trabalhando na pagina home
    
    * trabalhando no layout padrão
    
    * package.json
    
    * ...
    
    * arquivos adicionados
    
    * troquei a linguagem do html de "EN" para "PT-BR"
    
    * adicionei as cores padrão de texto e background no html
    
    * converti o svg para componente
    
    transformei esses svg em componentes para ter mais controle de personalização
    
    * criei um state para o titulo do layout
    
    criei esse state para alterar dinamicamente o titulo no meu componente de layout
    
    * assets cadastro
    
    * estrutura da página cadastro
    
    * icones
    
    * adicionei mais algumas classes utilitarias
    
    * layout principal finalizado
    
    * estilos do leyout principal
    
    * adicionei o atualizador do state de titulo do layout
    
    * redirecionamento
    
    * resetei o "margin" e "padding" global
    
    * layout e redirecionamento melhorado
    
    * [Correção] erro de digitação
    
    * steps
    
    * adicionei algumas classes utilitarias
    
    * trabalhando na pagina home
    
    * novos icones
    
    * novas classes para global.css
    
    * layout cadastro finalizado
    
    * margem no step
    
    * package-lock.json
    
    * Adição de pequenos detalhes no avatar
    
    * mockagem finalizada
    
    * margem entre botão e span
    
    * Pequenas correções (#9)
    
    -Renomeei a pasta "layout" para "layouts"
    -Renomeei o layout "Padrao" para "layoutDashboard"
    
    Co-authored-by: higor-d3v <hgrprofissional@gmail.com>
    Co-authored-by: Higor Souza <85526418+higor-d3v@users.noreply.github.com>

---
+++ src/layouts/layoutDashboard/index.jsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+import { Link, Outlet, useLocation } from "react-router-dom";
+
+import estilos from "./estilos.module.css";
+
+import HomeIcone from "../../assets/icones/home";
+import ClientesIcone from "../../assets/icones/clientes";
+import CobrancasIcone from "../../assets/icones/cobrancas";
+import chevronIcone from "../../assets/icones/chevron-down.svg";
+import editarIcone from "../../assets/icones/editar.svg";
+import sairIcone from "../../assets/icones/sair.svg";
+
+const Padrao = ({ tituloDaRota }) => {
+  const [menuUsuario, abreMenuUsuario] = useState(false);
+  const { pathname } = useLocation();
+
+  const tamanhoIconesMenu = 2.5;
+  const menuLinks = [
+    {
+      nome: "Home",
+      link: "/",
+      icone: <HomeIcone tamanho={tamanhoIconesMenu} />,
+    },
+    {
+      nome: "Clientes",
+      link: "/clientes",
+      icone: <ClientesIcone tamanho={tamanhoIconesMenu} />,
+    },
+    {
+      nome: "Cobranças",
+      link: "/cobrancas",
+      icone: <CobrancasIcone tamanho={tamanhoIconesMenu} />,
+    },
+  ];
+
+  return (
+    <div className={`${estilos.layout}`}>
+      <nav className={`${estilos.nav}`}>
+        <ul className={`${estilos.menu}`}>
+          {menuLinks.map((item) => (
+            <li key={item.nome} className={`${estilos.menuItem}`}>
+              <Link
+                to={item.link}
+                className={`
+                ${estilos.menuItemLink} 
+                ${item.link === pathname && estilos.menuItemLinkAtivo}`}
+              >
+                {item.icone}
+                <span>{item.nome}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <div className={`${estilos.main}`}>
+        <header className={`${estilos.header}`}>
+          <div className="container flex items-center">
+            <div className="flex-grow">
+              <h1 className={`${estilos.tituloDaRota}`}>{tituloDaRota}</h1>
+            </div>
+
+            <div className={`${estilos.usuarioContainer}`}>
+              <div
+                className={`flex items-center ${estilos.usuario}`}
+                onClick={() => abreMenuUsuario(!menuUsuario)}
+              >
+                <span className={`${estilos.avatar}`}>LR</span>
+                <span>Lorena</span>
+                <img src={chevronIcone} alt="" />
+              </div>
+
+              {menuUsuario && (
+                <div className={`${estilos.usuarioMenu}`}>
+                  <div className={`${estilos.usuarioMenuContainer}`}>
+                    <span className={`${estilos.usuarioMenuItem}`}>
+                      <img
+                        src={editarIcone}
+                        alt=""
+                        className={`${estilos.usuarioMenuIconeEditar}`}
+                      />
+                      <span>Editar</span>
+                    </span>
+
+                    <span className={`${estilos.usuarioMenuItem}`}>
+                      <img src={sairIcone} alt="" />
+                      <span>Sair</span>
+                    </span>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </header>
+
+        <div className="container">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Padrao;
--- src/rotas/index.jsx
+++ src/rotas/index.jsx
@@ -1,28 +1,50 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import LayoutPadrao from "../layout/Padrao";
+import LayoutDashboard from "../layouts/layoutDashboard";
+import LayoutCadastro from "../layouts/layoutCadastro";
 
 import Home from "../paginas/home";
 import Clientes from "../paginas/clientes";
 import Cobrancas from "../paginas/cobrancas";
 import Login from "../paginas/login";
-import Cadastro from "../paginas/cadastro";
+import CadastroInicio from "../paginas/cadastro/inicio";
+import CadastroSenha from "../paginas/cadastro/senha";
+import CadastroSucesso from "../paginas/cadastro/sucesso";
+import NotFound from "../paginas/notfound";
 
 const Rotas = () => {
+  const [tituloDaRota, setTituloDaRota] = useState("");
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<LayoutPadrao />}>
-          <Route path="/" element={<Home />} />
+        <Route element={<LayoutDashboard tituloDaRota={tituloDaRota} />}>
+          <Route
+            path="/"
+            element={<Home setTituloDaRota={setTituloDaRota} />}
+          />
 
-          <Route path="/clientes" element={<Clientes />} />
+          <Route
+            path="/clientes"
+            element={<Clientes setTituloDaRota={setTituloDaRota} />}
+          />
 
-          <Route path="/cobrancas" element={<Cobrancas />} />
+          <Route
+            path="/cobrancas"
+            element={<Cobrancas setTituloDaRota={setTituloDaRota} />}
+          />
         </Route>
 
         <Route path="/login" element={<Login />} />
 
-        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/cadastro" element={<LayoutCadastro />}>
+          <Route path="inicio" element={<CadastroInicio />} />
+          <Route path="senha" element={<CadastroSenha />} />
+          <Route path="sucesso" element={<CadastroSucesso />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
