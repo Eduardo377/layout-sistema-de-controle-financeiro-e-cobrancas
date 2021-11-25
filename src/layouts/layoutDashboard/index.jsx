03577c1ee3a7cbe8a193a657ae7c5cf117457375
Lucas Kennedy <lukakfp@gmail.com>
2021-11-25 15:46:15 -0300

    adição do modal do usuario

--- src/layouts/layoutDashboard/index.jsx
+++ src/layouts/layoutDashboard/index.jsx
@@ -3,6 +3,8 @@ import { Link, Outlet, useLocation } from "react-router-dom";
 
 import estilos from "./estilos.module.css";
 
+import Modal from "../../componentes/Modal";
+
 import HomeIcone from "../../assets/icones/home";
 import ClientesIcone from "../../assets/icones/clientes";
 import CobrancasIcone from "../../assets/icones/cobrancas";
@@ -12,6 +14,8 @@ import sairIcone from "../../assets/icones/sair.svg";
 
 const Padrao = ({ tituloDaRota }) => {
   const [menuUsuario, abreMenuUsuario] = useState(false);
+  const [modal, setModal] = useState(false);
+
   const { pathname } = useLocation();
 
   const tamanhoIconesMenu = 2.5;
@@ -34,70 +38,79 @@ const Padrao = ({ tituloDaRota }) => {
   ];
 
   return (
-    <div className={`${estilos.layout}`}>
-      <nav className={`${estilos.nav}`}>
-        <ul className={`${estilos.menu}`}>
-          {menuLinks.map((item) => (
-            <li key={item.nome} className={`${estilos.menuItem}`}>
-              <Link
-                to={item.link}
-                className={`
+    <>
+      <div className={`${estilos.layout}`}>
+        <nav className={`${estilos.nav}`}>
+          <ul className={`${estilos.menu}`}>
+            {menuLinks.map((item) => (
+              <li key={item.nome} className={`${estilos.menuItem}`}>
+                <Link
+                  to={item.link}
+                  className={`
                 ${estilos.menuItemLink} 
                 ${item.link === pathname && estilos.menuItemLinkAtivo}`}
-              >
-                {item.icone}
-                <span>{item.nome}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+                >
+                  {item.icone}
+                  <span>{item.nome}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
 
-      <div className={`${estilos.main}`}>
-        <header className={`${estilos.header}`}>
-          <div className="container flex items-center">
-            <div className="flex-grow">
-              <h1 className={`${estilos.tituloDaRota}`}>{tituloDaRota}</h1>
-            </div>
-
-            <div className={`${estilos.usuarioContainer}`}>
-              <div
-                className={`flex items-center ${estilos.usuario}`}
-                onClick={() => abreMenuUsuario(!menuUsuario)}
-              >
-                <span className={`${estilos.avatar}`}>LR</span>
-                <span>Lorena</span>
-                <img src={chevronIcone} alt="" />
+        <div className={`${estilos.main}`}>
+          <header className={`${estilos.header}`}>
+            <div className="container flex items-center">
+              <div className="flex-grow">
+                <h1 className={`${estilos.tituloDaRota}`}>{tituloDaRota}</h1>
               </div>
 
-              {menuUsuario && (
-                <div className={`${estilos.usuarioMenu}`}>
-                  <div className={`${estilos.usuarioMenuContainer}`}>
-                    <span className={`${estilos.usuarioMenuItem}`}>
-                      <img
-                        src={editarIcone}
-                        alt=""
-                        className={`${estilos.usuarioMenuIconeEditar}`}
-                      />
-                      <span>Editar</span>
-                    </span>
-
-                    <span className={`${estilos.usuarioMenuItem}`}>
-                      <img src={sairIcone} alt="" />
-                      <span>Sair</span>
-                    </span>
-                  </div>
+              <div className={`${estilos.usuarioContainer}`}>
+                <div
+                  className={`flex items-center ${estilos.usuario}`}
+                  onClick={() => abreMenuUsuario(!menuUsuario)}
+                >
+                  <span className={`${estilos.avatar}`}>LR</span>
+                  <span>Lorena</span>
+                  <img src={chevronIcone} alt="" />
                 </div>
-              )}
-            </div>
-          </div>
-        </header>
 
-        <div className="container">
-          <Outlet />
+                {menuUsuario && (
+                  <div className={`${estilos.usuarioMenu}`}>
+                    <div className={`${estilos.usuarioMenuContainer}`}>
+                      <span
+                        className={`${estilos.usuarioMenuItem}`}
+                        onClick={() => setModal(true)}
+                      >
+                        <img
+                          src={editarIcone}
+                          alt=""
+                          className={`${estilos.usuarioMenuIconeEditar}`}
+                        />
+                        <span>Editar</span>
+                      </span>
+
+                      <span className={`${estilos.usuarioMenuItem}`}>
+                        <img src={sairIcone} alt="" />
+                        <span>Sair</span>
+                      </span>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+          </header>
+
+          <div className="container">
+            <Outlet />
+          </div>
         </div>
       </div>
-    </div>
+
+      <Modal modal={modal} handleModal={setModal}>
+        Modal
+      </Modal>
+    </>
   );
 };
 
