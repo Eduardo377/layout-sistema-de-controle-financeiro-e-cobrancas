25d5ca80a8f4461b7b74b7c32e8b945ce932a74f
Lucas Kennedy <lukakfp@gmail.com>
2021-11-25 16:20:04 -0300

    Refatoração da pagina clientes
    isolei o formulario de castro de cliente em um componente dentro da pasta clientes

---
+++ src/paginas/clientes/Formulario/index.jsx
@@ -0,0 +1,131 @@
+import React from "react";
+import estilos from "./estilos.module.css";
+
+const Formulario = () => {
+  function onSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="mb-1">
+        <label htmlFor="nome">Nome*</label>
+        <input
+          type="text"
+          id="nome"
+          name="nome"
+          placeholder="Digite o nome"
+          required
+        />
+      </div>
+
+      <div className="mb-1">
+        <label htmlFor="email">Email*</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Digite o email"
+          required
+        />
+      </div>
+
+      <div className="flex gap-1 mb-1">
+        <div>
+          <label htmlFor="cpf">CPF*</label>
+          <input
+            type="text"
+            id="cpf"
+            name="cpf"
+            placeholder="Digite o cpf"
+            required
+            inputMode="numeric"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="telefone">Telefone*</label>
+          <input
+            type="tel"
+            id="telefone"
+            name="telefone"
+            placeholder="Digite o telefone"
+            required
+            inputMode="numeric"
+          />
+        </div>
+      </div>
+
+      <div className="mb-1">
+        <label htmlFor="endereco">Endereço</label>
+        <input
+          type="text"
+          id="endereco"
+          name="endereco"
+          placeholder="Digite o endereço"
+        />
+      </div>
+
+      <div className="mb-1">
+        <label htmlFor="complemento">Complemento</label>
+        <input
+          type="text"
+          id="complemento"
+          name="complemento"
+          placeholder="Digite o complemento"
+        />
+      </div>
+
+      <div className="flex gap-1 mb-1">
+        <div>
+          <label htmlFor="cep">CEP</label>
+          <input
+            type="text"
+            id="cep"
+            name="cep"
+            placeholder="Digite o cep"
+            inputMode="numeric"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="bairro">Bairro</label>
+          <input
+            type="text"
+            id="bairro"
+            name="bairro"
+            placeholder="Digite o bairro"
+          />
+        </div>
+      </div>
+
+      <div className="flex gap-1 mb-2">
+        <div className="flex-1">
+          <label htmlFor="cidade">Cidade</label>
+          <input
+            type="text"
+            id="cidade"
+            name="cidade"
+            placeholder="Digite a cidade"
+          />
+        </div>
+
+        <div className={`${estilos.inputContainerUF}`}>
+          <label htmlFor="uf">UF</label>
+          <input type="text" id="uf" name="uf" placeholder="Digite a UF" />
+        </div>
+      </div>
+
+      <div className="flex gap-2">
+        <button type="reset" className="btn-secundario flex-1">
+          Cancelar
+        </button>
+        <button type="submit" className="btn-primario flex-1">
+          Aplicar
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Formulario;
--- src/paginas/clientes/index.jsx
+++ src/paginas/clientes/index.jsx
@@ -4,7 +4,9 @@ import estilos from "./estilos.module.css";
 
 import ClientesIcone from "../../assets/icones/clientes";
 import lupaIcone from "../../assets/icones/lupa.svg";
+
 import Modal from "../../componentes/Modal";
+import Formulario from "./Formulario";
 
 const Clientes = ({ setTituloDaRota }) => {
   const [modal, setModal] = useState(false);
@@ -48,128 +50,7 @@ const Clientes = ({ setTituloDaRota }) => {
           <h3>Cadastro do cliente</h3>
         </div>
 
-        <form onSubmit={onSubmit}>
-          <div className="mb-1">
-            <label htmlFor="nome">Nome*</label>
-            <input
-              type="text"
-              id="nome"
-              name="nome"
-              placeholder="Digite o nome"
-              required
-            />
-          </div>
-
-          <div className="mb-1">
-            <label htmlFor="email">Email*</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Digite o email"
-              required
-            />
-          </div>
-
-          <div className="flex gap-1 mb-1">
-            <div>
-              <label htmlFor="cpf">CPF*</label>
-              <input
-                type="text"
-                id="cpf"
-                name="cpf"
-                placeholder="Digite o cpf"
-                required
-                inputMode="numeric"
-              />
-            </div>
-
-            <div>
-              <label htmlFor="telefone">Telefone*</label>
-              <input
-                type="tel"
-                id="telefone"
-                name="telefone"
-                placeholder="Digite o telefone"
-                required
-                inputMode="numeric"
-              />
-            </div>
-          </div>
-
-          <div className="mb-1">
-            <label htmlFor="endereco">Endereço</label>
-            <input
-              type="text"
-              id="endereco"
-              name="endereco"
-              placeholder="Digite o endereço"
-            />
-          </div>
-
-          <div className="mb-1">
-            <label htmlFor="complemento">Complemento</label>
-            <input
-              type="text"
-              id="complemento"
-              name="complemento"
-              placeholder="Digite o complemento"
-            />
-          </div>
-
-          <div className="flex gap-1 mb-1">
-            <div>
-              <label htmlFor="cep">CEP</label>
-              <input
-                type="text"
-                id="cep"
-                name="cep"
-                placeholder="Digite o cep"
-                inputMode="numeric"
-              />
-            </div>
-
-            <div>
-              <label htmlFor="bairro">Bairro</label>
-              <input
-                type="text"
-                id="bairro"
-                name="bairro"
-                placeholder="Digite o bairro"
-              />
-            </div>
-          </div>
-
-          <div className="flex gap-1 mb-2">
-            <div className="flex-1">
-              <label htmlFor="cidade">Cidade</label>
-              <input
-                type="text"
-                id="cidade"
-                name="cidade"
-                placeholder="Digite a cidade"
-              />
-            </div>
-
-            <div className={`${estilos.inputContainerUF}`}>
-              <label htmlFor="uf">UF</label>
-              <input type="text" id="uf" name="uf" placeholder="Digite a UF" />
-            </div>
-          </div>
-
-          <div className="flex gap-2">
-            <button
-              type="reset"
-              className="btn-secundario flex-1"
-              onClick={() => setModal(false)}
-            >
-              Cancelar
-            </button>
-            <button type="submit" className="btn-primario flex-1">
-              Aplicar
-            </button>
-          </div>
-        </form>
+        <Formulario />
       </Modal>
     </div>
   );
