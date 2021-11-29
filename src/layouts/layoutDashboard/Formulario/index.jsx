27ddaf6174276d7e2b86df64e8374fd4228d9fa3
Lucas Kennedy <lukakfp@gmail.com>
2021-11-28 21:59:51 -0300

    Form edicao usuario (#27)
    * adicionei pacote para detecção de clicks fora de um elemento
    
    * refatoração layout dashboard
    
    desaclopei alguns elementos afim de isolar suas competencias
    
    * utilitario para input em estado de erro
    
    * .vscode
    
    * ...
    
    * ...
    
    * validação de campos formulario clientes
    
    * atualizando a tela com informações do banco
    
    * cadastro de clientes
    
    * .vscode
    
    * correção do elemento que recebe o click
    
    * utilitario para botao
    
    * adicionei estados de erros, sucesso e carregando

--- src/layouts/layoutDashboard/Formulario/index.jsx
+++ src/layouts/layoutDashboard/Formulario/index.jsx
@@ -8,9 +8,17 @@ import estilos from "./estilos.module.css";
 
 import olhoFechado from "../../../assets/icones/olho.svg";
 import olhoAberto from "../../../assets/icones/olho2.png";
+import sucessoIcone from "../../../assets/icones/sucesso.svg";
 
 const Formulario = ({ usuario, setUsuario, setModal }) => {
   const [token] = useLocalStorage("token");
+  const [erroEmailExiste, setErroEmailExiste] = useState(false);
+  const [erroCpfExiste, setErroCpfExiste] = useState(false);
+  const [carregando, setCarregando] = useState(false);
+  const [verSenha, setVerSenha] = useState("password");
+  const [iconeSenha, setIconeSenha] = useState(olhoFechado);
+  const [senhaValida, setSenhaValida] = useState(true);
+  const [sucesso, setSucesso] = useState(false);
 
   const {
     register,
@@ -19,10 +27,6 @@ const Formulario = ({ usuario, setUsuario, setModal }) => {
   } = useForm({
     resolver: yupResolver(validacao),
   });
-
-  const [verSenha, setVerSenha] = useState("password");
-  const [iconeSenha, setIconeSenha] = useState(olhoFechado);
-  const [senhaValida, setSenhaValida] = useState(true);
 
   useEffect(() => {
     setIconeSenha(verSenha === "password" ? olhoFechado : olhoAberto);
@@ -41,6 +45,7 @@ const Formulario = ({ usuario, setUsuario, setModal }) => {
   }
 
   async function onSubmit(data) {
+    setCarregando(true);
     const { senha1, senha2, ...resto } = data;
 
     if (senha1 || senha2) {
@@ -68,120 +73,145 @@ const Formulario = ({ usuario, setUsuario, setModal }) => {
         throw responseData;
       }
 
-      setModal(false);
       setUsuario(dadosASerAtualizado);
+      setCarregando(false);
+      setSucesso(true);
     } catch (error) {
-      alert(error.message);
+      if (error.message === "O email já existe" || error.field === "email") {
+        setErroEmailExiste(true);
+      }
+
+      console.log(error);
+      setCarregando(false);
     }
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="mb-1">
-        <label htmlFor="nome">Nome*</label>
-        <input
-          defaultValue={usuario?.nome}
-          id="nome"
-          name="nome"
-          placeholder="Digite seu nome"
-          {...register("nome", {
-            required: true,
-          })}
-          className={`${errors.nome && " inputErro"}`}
-        />
+    <>
+      {!sucesso && (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-1">
+            <label htmlFor="nome">Nome*</label>
+            <input
+              defaultValue={usuario?.nome}
+              id="nome"
+              name="nome"
+              placeholder="Digite seu nome"
+              {...register("nome", {
+                required: true,
+              })}
+              className={`${errors.nome && " inputErro"}`}
+            />
 
-        <p className={`${"inputMensagemErro"}`}>{errors.nome?.message}</p>
-      </div>
+            <p className={`${"inputMensagemErro"}`}>{errors.nome?.message}</p>
+          </div>
 
-      <div className="mb-1">
-        <label htmlFor="email">Email*</label>
-        <input
-          defaultValue={usuario?.email}
-          id="email"
-          name="email"
-          placeholder="Digite seu email"
-          {...register("email", { required: true })}
-          className={`${errors.email && "inputErro"}`}
-        />
-        <p className={`${"inputMensagemErro"}`}>{errors.email?.message}</p>
-      </div>
+          <div className="mb-1">
+            <label htmlFor="email">Email*</label>
+            <input
+              defaultValue={usuario?.email}
+              id="email"
+              name="email"
+              placeholder="Digite seu email"
+              {...register("email", { required: true })}
+              onChange={() => setErroEmailExiste(false)}
+              className={`${errors.email && "inputErro"}`}
+            />
+            <p className={`${"inputMensagemErro"}`}>{errors.email?.message}</p>
+            <p className={`${"inputMensagemErro"}`}>
+              {erroEmailExiste && "E-mail já cadastrado."}
+            </p>
+          </div>
 
-      <div className="flex gap-2 mb-1">
-        <div>
-          <label htmlFor="cpf">CPF</label>
-          <input
-            defaultValue={usuario?.cpf}
-            type="text"
-            id="cpf"
-            name="cpf"
-            placeholder="Digite seu CPF"
-            {...register("cpf")}
-          />
+          <div className="flex gap-2 mb-1">
+            <div>
+              <label htmlFor="cpf">CPF</label>
+              <input
+                defaultValue={usuario?.cpf}
+                type="text"
+                id="cpf"
+                name="cpf"
+                placeholder="Digite seu CPF"
+                {...register("cpf")}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="tel">Telefone</label>
+              <input
+                defaultValue={usuario?.tel}
+                type="tel"
+                id="tel"
+                name="tel"
+                placeholder="Digite seu telefone"
+                {...register("tel")}
+              />
+            </div>
+          </div>
+
+          <div className="mb-1">
+            <label htmlFor="novasenha">Nova Senha</label>
+            <div className={`${estilos.inputContainer}`}>
+              <input
+                type={verSenha}
+                id="novasenha"
+                name="novasenha"
+                placeholder=""
+                {...register("senha1")}
+                onChange={() => setSenhaValida(true)}
+                className={`${!senhaValida && "inputErro"}`}
+              />
+              <img
+                src={iconeSenha}
+                alt=""
+                className={`${estilos.inputContainerIcone}`}
+                onClick={handleVerSenha}
+              />
+            </div>
+          </div>
+
+          <div className="mb-1">
+            <label htmlFor="confirmarsenha">Confirmar Senha</label>
+            <div className={`${estilos.inputContainer}`}>
+              <input
+                type={verSenha}
+                id="confirmarsenha"
+                name="confirmarsenha"
+                placeholder=""
+                {...register("senha2")}
+                onChange={() => setSenhaValida(true)}
+                className={`${!senhaValida && "inputErro"}`}
+              />
+
+              <img
+                src={iconeSenha}
+                alt=""
+                className={`${estilos.inputContainerIcone}`}
+                onClick={handleVerSenha}
+              />
+            </div>
+            <p className={`${"inputMensagemErro"}`}>
+              {!senhaValida && "As senhas não coincidem"}
+            </p>
+          </div>
+
+          <div className="text-center">
+            <button
+              className={`btn-primario ${carregando && "btn-desabilitado"}`}
+            >
+              {carregando ? "Carregando..." : "Aplicar"}
+            </button>
+          </div>
+        </form>
+      )}
+
+      {sucesso && (
+        <div className="flex-column items-center gap-2">
+          <img src={sucessoIcone} alt="Sucesso" />
+          <h3>Cadastro atualizado com sucesso!</h3>
         </div>
-
-        <div>
-          <label htmlFor="tel">Telefone</label>
-          <input
-            defaultValue={usuario?.tel}
-            type="tel"
-            id="tel"
-            name="tel"
-            placeholder="Digite seu telefone"
-            {...register("tel")}
-          />
-        </div>
-      </div>
-
-      <div className="mb-1">
-        <label htmlFor="novasenha">Nova Senha</label>
-        <div className={`${estilos.inputContainer}`}>
-          <input
-            type={verSenha}
-            id="novasenha"
-            name="novasenha"
-            placeholder=""
-            {...register("senha1")}
-            onChange={() => setSenhaValida(true)}
-            className={`${!senhaValida && "inputErro"}`}
-          />
-          <img
-            src={iconeSenha}
-            alt=""
-            className={`${estilos.inputContainerIcone}`}
-            onClick={handleVerSenha}
-          />
-        </div>
-      </div>
-
-      <div className="mb-1">
-        <label htmlFor="confirmarsenha">Confirmar Senha</label>
-        <div className={`${estilos.inputContainer}`}>
-          <input
-            type={verSenha}
-            id="confirmarsenha"
-            name="confirmarsenha"
-            placeholder=""
-            {...register("senha2")}
-            onChange={() => setSenhaValida(true)}
-            className={`${!senhaValida && "inputErro"}`}
-          />
-
-          <img
-            src={iconeSenha}
-            alt=""
-            className={`${estilos.inputContainerIcone}`}
-            onClick={handleVerSenha}
-          />
-        </div>
-        <p className={`${"inputMensagemErro"}`}>
-          {!senhaValida && "As senhas não coincidem"}
-        </p>
-      </div>
-
-      <div className="text-center">
-        <button className="btn-primario">Aplicar</button>
-      </div>
-    </form>
+      )}
+    </>
   );
 };
 
