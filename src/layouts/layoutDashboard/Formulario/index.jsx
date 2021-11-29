db50a34f07f97781796e4d89593ac5ad1ffbfffc
Lucas Kennedy <lukakfp@gmail.com>
2021-11-28 23:12:07 -0300

    fix main

--- src/layouts/layoutDashboard/Formulario/index.jsx
+++ src/layouts/layoutDashboard/Formulario/index.jsx
@@ -13,17 +13,12 @@ import sucessoIcone from "../../../assets/icones/sucesso.svg";
 const Formulario = ({ usuario, setUsuario, setModal }) => {
   const [token] = useLocalStorage("token");
   const [erroEmailExiste, setErroEmailExiste] = useState(false);
-  const [erroCpfExiste, setErroCpfExiste] = useState(false);
+  // const [erroCpfExiste, setErroCpfExiste] = useState(false);
   const [carregando, setCarregando] = useState(false);
   const [verSenha, setVerSenha] = useState("password");
   const [iconeSenha, setIconeSenha] = useState(olhoFechado);
   const [senhaValida, setSenhaValida] = useState(true);
   const [sucesso, setSucesso] = useState(false);
-
-
-const Formulario = ({ usuario, setUsuario, setModal }) => {
-  const [token] = useLocalStorage("token");
-
 
   const {
     register,
@@ -50,9 +45,7 @@ const Formulario = ({ usuario, setUsuario, setModal }) => {
   }
 
   async function onSubmit(data) {
-
     setCarregando(true);
-
     const { senha1, senha2, ...resto } = data;
 
     if (senha1 || senha2) {
@@ -80,7 +73,6 @@ const Formulario = ({ usuario, setUsuario, setModal }) => {
         throw responseData;
       }
 
-
       setUsuario(dadosASerAtualizado);
       setCarregando(false);
       setSucesso(true);
@@ -90,17 +82,10 @@ const Formulario = ({ usuario, setUsuario, setModal }) => {
       }
 
       setCarregando(false);
-
-      setModal(false);
-      setUsuario(dadosASerAtualizado);
-    } catch (error) {
-      alert(error.message);
-
     }
   }
 
   return (
-
     <>
       {!sucesso && (
         <form onSubmit={handleSubmit(onSubmit)}>
@@ -223,104 +208,6 @@ const Formulario = ({ usuario, setUsuario, setModal }) => {
         <div className="flex-column items-center gap-2">
           <img src={sucessoIcone} alt="Sucesso" />
           <h3>Cadastro atualizado com sucesso!</h3>
-=======
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="mb-1">
-        <label htmlFor="nome">Nome*</label>
-        <input
-          defaultValue={usuario?.nome}
-          id="nome"
-          name="nome"
-          placeholder="Digite seu nome"
-          {...register("nome", {
-            required: true,
-          })}
-          className={`${errors.nome && " inputErro"}`}
-        />
-
-        <p className={`${"inputMensagemErro"}`}>{errors.nome?.message}</p>
-      </div>
-
-      <div className="mb-1">
-        <label htmlFor="email">Email*</label>
-        <input
-          defaultValue={usuario?.email}
-          id="email"
-          name="email"
-          placeholder="Digite seu email"
-          {...register("email", { required: true })}
-          className={`${errors.email && "inputErro"}`}
-        />
-        <p className={`${"inputMensagemErro"}`}>{errors.email?.message}</p>
-      </div>
-
-      <div className="flex gap-2 mb-1">
-        <div>
-          <label htmlFor="cpf">CPF</label>
-          <input
-            defaultValue={usuario?.cpf}
-            type="text"
-            id="cpf"
-            name="cpf"
-            placeholder="Digite seu CPF"
-            {...register("cpf")}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="tel">Telefone</label>
-          <input
-            defaultValue={usuario?.tel}
-            type="tel"
-            id="tel"
-            name="tel"
-            placeholder="Digite seu telefone"
-            {...register("tel")}
-          />
-        </div>
-      </div>
-
-      <div className="mb-1">
-        <label htmlFor="novasenha">Nova Senha</label>
-        <div className={`${estilos.inputContainer}`}>
-          <input
-            type={verSenha}
-            id="novasenha"
-            name="novasenha"
-            placeholder=""
-            {...register("senha1")}
-            onChange={() => setSenhaValida(true)}
-            className={`${!senhaValida && "inputErro"}`}
-          />
-          <img
-            src={iconeSenha}
-            alt=""
-            className={`${estilos.inputContainerIcone}`}
-            onClick={handleVerSenha}
-          />
-        </div>
-      </div>
-
-      <div className="mb-1">
-        <label htmlFor="confirmarsenha">Confirmar Senha</label>
-        <div className={`${estilos.inputContainer}`}>
-          <input
-            type={verSenha}
-            id="confirmarsenha"
-            name="confirmarsenha"
-            placeholder=""
-            {...register("senha2")}
-            onChange={() => setSenhaValida(true)}
-            className={`${!senhaValida && "inputErro"}`}
-          />
-
-          <img
-            src={iconeSenha}
-            alt=""
-            className={`${estilos.inputContainerIcone}`}
-            onClick={handleVerSenha}
-          />
-
         </div>
       )}
     </>
