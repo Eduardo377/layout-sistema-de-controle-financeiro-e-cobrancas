c1a123f35d3e293c91c1ac1025468adbca1cee91
Lucas Kennedy <lukakfp@gmail.com>
2021-11-29 00:29:08 -0300

    cadastro clientes

--- src/paginas/clientes/Formulario/index.jsx
+++ src/paginas/clientes/Formulario/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocalStorage } from "react-use";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -6,8 +6,11 @@ import validacao from "./validacao";
 
 import estilos from "./estilos.module.css";
 
-const Formulario = ({ setModal }) => {
+const Formulario = ({ setModal, setAlerta }) => {
   const [token] = useLocalStorage("token");
+  const [erroEmailExiste, setErroEmailExiste] = useState(false);
+  // const [erroCpfExiste, setErroCpfExiste] = useState(false);
+  const [carregando, setCarregando] = useState(false);
   const {
     register,
     handleSubmit,
@@ -16,7 +19,9 @@ const Formulario = ({ setModal }) => {
     resolver: yupResolver(validacao),
   });
 
-  async function onSubmit(data) {
+  async function cadastrarCliente(data) {
+    setCarregando(true);
+
     console.log(data);
 
     try {
@@ -39,140 +44,158 @@ const Formulario = ({ setModal }) => {
       }
 
       setModal(false);
+      setAlerta(true);
+      console.log(responseData);
     } catch (error) {
-      alert(error);
+      console.log(error);
+      setCarregando(false);
     }
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="mb-1">
-        <label htmlFor="nome">Nome*</label>
-        <input
-          id="nome"
-          name="nome"
-          placeholder="Digite o nome"
-          {...register("nome")}
-          className={`${errors.nome && "inputErro"}`}
-        />
-        <p className={`${"inputMensagemErro"}`}>{errors.nome?.message}</p>
-      </div>
-
-      <div className="mb-1">
-        <label htmlFor="email">Email*</label>
-        <input
-          id="email"
-          name="email"
-          placeholder="Digite o email"
-          {...register("email")}
-          className={`${errors.email && "inputErro"}`}
-        />
-        <p className={`${"inputMensagemErro"}`}>{errors.email?.message}</p>
-      </div>
-
-      <div className="flex gap-1 mb-1">
-        <div>
-          <label htmlFor="cpf">CPF*</label>
+    <>
+      <form onSubmit={handleSubmit(cadastrarCliente)}>
+        <div className="mb-1">
+          <label htmlFor="nome">Nome*</label>
           <input
-            id="cpf"
-            name="cpf"
-            placeholder="Digite o cpf"
-            {...register("cpf")}
-            className={`${errors.cpf && "inputErro"}`}
+            id="nome"
+            name="nome"
+            placeholder="Digite o nome"
+            {...register("nome", {
+              required: true,
+            })}
+            className={`${errors.nome && "inputErro"}`}
           />
-          <p className={`${"inputMensagemErro"}`}>{errors.cpf?.message}</p>
+          <p className={`${"inputMensagemErro"}`}>{errors.nome?.message}</p>
         </div>
 
-        <div>
-          <label htmlFor="telefone">Telefone*</label>
+        <div className="mb-1">
+          <label htmlFor="email">Email*</label>
           <input
-            id="telefone"
-            name="telefone"
-            placeholder="Digite o telefone"
-            {...register("telefone")}
-            className={`${errors.telefone && "inputErro"}`}
+            id="email"
+            name="email"
+            placeholder="Digite o email"
+            {...register("email")}
+            className={`${errors.email && "inputErro"}`}
           />
-          <p className={`${"inputMensagemErro"}`}>{errors.telefone?.message}</p>
+          <p className={`${"inputMensagemErro"}`}>{errors.email?.message}</p>
         </div>
-      </div>
 
-      <div className="mb-1">
-        <label htmlFor="endereco">Endereço</label>
-        <input
-          id="endereco"
-          name="endereco"
-          placeholder="Digite o endereço"
-          {...register("endereco")}
-        />
-      </div>
+        <div className="flex gap-1 mb-1">
+          <div>
+            <label htmlFor="cpf">CPF*</label>
+            <input
+              id="cpf"
+              name="cpf"
+              placeholder="Digite o cpf"
+              {...register("cpf")}
+              className={`${errors.cpf && "inputErro"}`}
+            />
+            <p className={`${"inputMensagemErro"}`}>{errors.cpf?.message}</p>
+          </div>
 
-      <div className="mb-1">
-        <label htmlFor="complemento">Complemento</label>
-        <input
-          id="complemento"
-          name="complemento"
-          placeholder="Digite o complemento"
-          {...register("complemento")}
-        />
-      </div>
+          <div>
+            <label htmlFor="telefone">Telefone*</label>
+            <input
+              id="telefone"
+              name="telefone"
+              placeholder="Digite o telefone"
+              {...register("telefone")}
+              className={`${errors.telefone && "inputErro"}`}
+            />
+            <p className={`${"inputMensagemErro"}`}>
+              {errors.telefone?.message}
+            </p>
+          </div>
+        </div>
 
-      <div className="flex gap-1 mb-1">
-        <div>
-          <label htmlFor="cep">CEP</label>
+        <div className="mb-1">
+          <label htmlFor="endereco">Endereço</label>
           <input
-            id="cep"
-            name="cep"
-            placeholder="Digite o cep"
-            {...register("cep")}
+            id="endereco"
+            name="endereco"
+            placeholder="Digite o endereço"
+            {...register("endereco")}
           />
         </div>
 
-        <div>
-          <label htmlFor="bairro">Bairro</label>
+        <div className="mb-1">
+          <label htmlFor="complemento">Complemento</label>
           <input
-            id="bairro"
-            name="bairro"
-            placeholder="Digite o bairro"
-            {...register("bairro")}
+            id="complemento"
+            name="complemento"
+            placeholder="Digite o complemento"
+            {...register("complemento")}
           />
         </div>
-      </div>
 
-      <div className="flex gap-1 mb-2">
-        <div className="flex-1">
-          <label htmlFor="cidade">Cidade*</label>
-          <input
-            id="cidade"
-            name="cidade"
-            placeholder="Digite a cidade"
-            {...register("cidade")}
-            className={`${errors.cidade && "inputErro"}`}
-          />
-          <p className={`${"inputMensagemErro"}`}>{errors.cidade?.message}</p>
+        <div className="flex gap-1 mb-1">
+          <div>
+            <label htmlFor="cep">CEP</label>
+            <input
+              id="cep"
+              name="cep"
+              placeholder="Digite o cep"
+              {...register("cep")}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="bairro">Bairro</label>
+            <input
+              id="bairro"
+              name="bairro"
+              placeholder="Digite o bairro"
+              {...register("bairro")}
+            />
+          </div>
         </div>
 
-        <div className={`${estilos.inputContainerUF}`}>
-          <label htmlFor="uf">UF*</label>
-          <input
-            id="uf"
-            name="uf"
-            placeholder="Digite a UF"
-            {...register("uf")}
-            className={`${errors.uf && "inputErro"}`}
-          />
-          <p className={`${"inputMensagemErro"}`}>{errors.uf?.message}</p>
-        </div>
-      </div>
+        <div className="flex gap-1 mb-2">
+          <div className="flex-1">
+            <label htmlFor="cidade">Cidade*</label>
+            <input
+              id="cidade"
+              name="cidade"
+              placeholder="Digite a cidade"
+              {...register("cidade")}
+              className={`${errors.cidade && "inputErro"}`}
+            />
+            <p className={`${"inputMensagemErro"}`}>{errors.cidade?.message}</p>
+          </div>
 
-      <div className="flex gap-2">
-        <button type="reset" className="btn-secundario flex-1">
-          Cancelar
-        </button>
-        <button type="submit" className="btn-primario flex-1">
-          Aplicar
-        </button>
-      </div>
-    </form>
+          <div className={`${estilos.inputContainerUF}`}>
+            <label htmlFor="uf">UF*</label>
+            <input
+              id="uf"
+              name="uf"
+              placeholder="Digite a UF"
+              {...register("uf")}
+              className={`${errors.uf && "inputErro"}`}
+            />
+            <p className={`${"inputMensagemErro"}`}>{errors.uf?.message}</p>
+          </div>
+        </div>
+
+        <div className="flex gap-2">
+          <button
+            type="reset"
+            className="btn-secundario flex-1"
+            onClick={() => setModal(false)}
+          >
+            Cancelar
+          </button>
+          <button
+            type="submit"
+            className={`btn-primario flex-1 ${
+              carregando && "btn-desabilitado"
+            }`}
+          >
+            {carregando ? "Carregando..." : "Aplicar"}
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
--- src/paginas/clientes/index.jsx
+++ src/paginas/clientes/index.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 
 import estilos from "./estilos.module.css";
 
+import Alerta from "../../componentes/Alerta";
+
 import ClientesIcone from "../../assets/icones/clientes";
 import lupaIcone from "../../assets/icones/lupa.svg";
 
@@ -10,6 +12,7 @@ import Formulario from "./Formulario";
 
 const Clientes = ({ setTituloDaRota }) => {
   const [modal, setModal] = useState(false);
+  const [alerta, setAlerta] = useState(false);
 
   useEffect(() => {
     setTituloDaRota("Controle de clientes");
@@ -20,29 +23,31 @@ const Clientes = ({ setTituloDaRota }) => {
   }
 
   return (
-    <div className={`${estilos.clientesSecao}`}>
-      <section className={`flex items-center`}>
-        <div className={`flex items-center flex-1 gap-1`}>
-          <ClientesIcone tamanho={2} />
-          <h2>Clientes</h2>
-        </div>
-
-        <div className={`flex  gap-1`}>
-          <button className="btn-primario" onClick={() => setModal(true)}>
-            + Adicionar cliente
-          </button>
-          <div className={`${estilos.inputContainer}`}>
-            <input type="text" placeholder="Pesquisa" />
-            <img
-              src={lupaIcone}
-              alt="lupa"
-              className={`${estilos.inputIcone}`}
-            />
+    <>
+      <div className={`${estilos.clientesSecao}`}>
+        <section className={`flex items-center`}>
+          <div className={`flex items-center flex-1 gap-1`}>
+            <ClientesIcone tamanho={2} />
+            <h2>Clientes</h2>
           </div>
-        </div>
-      </section>
 
-      <section></section>
+          <div className={`flex  gap-1`}>
+            <button className="btn-primario" onClick={() => setModal(true)}>
+              + Adicionar cliente
+            </button>
+            <div className={`${estilos.inputContainer}`}>
+              <input type="text" placeholder="Pesquisa" />
+              <img
+                src={lupaIcone}
+                alt="lupa"
+                className={`${estilos.inputIcone}`}
+              />
+            </div>
+          </div>
+        </section>
+
+        <section></section>
+      </div>
 
       <Modal modal={modal} handleModal={setModal}>
         <div className="flex gap-1 items-center mb-2">
@@ -50,9 +55,16 @@ const Clientes = ({ setTituloDaRota }) => {
           <h3>Cadastro do cliente</h3>
         </div>
 
-        <Formulario setModal={setModal} />
+        <Formulario setModal={setModal} setAlerta={setAlerta} />
       </Modal>
-    </div>
+
+      {alerta && (
+        <Alerta
+          mensagem="Cadastro concluído com sucesso"
+          handleAlerta={setAlerta}
+        />
+      )}
+    </>
   );
 };
 
