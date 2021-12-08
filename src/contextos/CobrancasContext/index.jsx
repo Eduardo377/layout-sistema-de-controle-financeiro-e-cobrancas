ebbaa9389ead7c2d48ba61812e107190072d5b29
Lucas Kennedy <lukakfp@gmail.com>
2021-12-08 19:09:39 -0300

    Criado contexto para cobranças

---
+++ src/contextos/CobrancasContext/index.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import fetcher from "constantes/fetcher";
+import notify from "constantes/notify";
+import { Outlet } from "react-router";
+
+const CobrancasContext = React.createContext([]);
+
+const CobrancasContextProvider = () => {
+  const [cobrancas, setCobrancas] = React.useState([]);
+  const [loadingCobrancas, setLoadingCobrancas] = React.useState(true);
+
+  const [cobrancasCLiente, setCobrancasCLiente] = React.useState([]);
+  const [loadingCobrancasCLiente, setLoadingCobrancasCLiente] =
+    React.useState(true);
+
+  React.useEffect(() => {
+    (async function () {
+      try {
+        cobrancas(await buscaCobrancas());
+
+        setLoadingCobrancas(false);
+      } catch (error) {
+        setLoadingCobrancas(false);
+      }
+    })();
+  }, [cobrancas, cobrancasCLiente]);
+
+  return (
+    <CobrancasContext.Provider
+      value={{
+        cobrancas,
+        setCobrancas,
+        loadingCobrancas,
+        setLoadingCobrancas,
+        cobrancasCLiente,
+        setCobrancasCLiente,
+        loadingCobrancasCLiente,
+        setLoadingCobrancasCLiente,
+      }}
+    >
+      <Outlet />
+    </CobrancasContext.Provider>
+  );
+};
+
+async function buscaCobrancas() {
+  try {
+    const response = await fetcher("cobrancas");
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    notify.erro(error.message).showToast();
+  }
+}
+
+export default CobrancasContext;
+export { CobrancasContextProvider };
