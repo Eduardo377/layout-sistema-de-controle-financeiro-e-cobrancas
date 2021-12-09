3e603cc42818d672e0144efc4e00068f48679678
Lucas Kennedy <lukakfp@gmail.com>
2021-12-09 11:04:32 -0300

    Correção no valor

--- src/componentes/FormularioCobrancas/defaultValues.js
+++ src/componentes/FormularioCobrancas/defaultValues.js
@@ -2,7 +2,7 @@ const defaultValues = (cobranca) => {
   return {
     id: cobranca.id || "",
     descricao: cobranca.descricao || "",
-    valor: cobranca.valor || "",
+    valor: cobranca.valor / 100 || "",
     paga: cobranca.paga || true,
     data_vencimento: cobranca.data_vencimento || "",
   };
