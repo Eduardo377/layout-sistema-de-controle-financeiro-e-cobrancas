fda10151e3e732f62791c4a0c1b465afacb1fb9c
Lucas Kennedy <lukakfp@gmail.com>
2021-12-10 22:52:53 -0300

    Refatoração  e populando componente

--- src/paginas/home/CardClientes/index.jsx
+++ src/paginas/home/CardClientes/index.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import estilos from "./estilos.module.css";
-
+import ClientesContext from "contextos/ClientesContext";
 import clienteVerde from "@/assets/icones/cliente-verde.svg";
 import clienteVermelho from "@/assets/icones/cliente-vermelho.svg";
+import { useEffect } from "react/cjs/react.development";
 
-const CardCobrancas = ({ nome = "Nome do card", total, cor, lista }) => {
+const CardClientes = ({ nome = "Nome do card", total, cor, lista, status }) => {
+  const { clientes } = useContext(ClientesContext);
+  // const [inadimplentes, setInadimplentes] = useState([])
+  // const [emDia, setEmdia] = useState([])
+  const [clientesPorStatus, setClientesPorStatus] = useState([]);
+
+  useEffect(() => {
+    if (clientes.length > 0) {
+      const clientesFiltrado = clientes.filter(
+        (cliente) => cliente.inadimplente === status
+      );
+
+      setClientesPorStatus(clientesFiltrado);
+
+      console.log(clientesFiltrado);
+    }
+  }, [clientes]);
+
   return (
     <div className={`flex-1 ${estilos.card}`}>
       <div className={`flex items-center gap-1 ${estilos.header}`}>
@@ -13,39 +31,46 @@ const CardCobrancas = ({ nome = "Nome do card", total, cor, lista }) => {
           {nome}
         </span>
         <span className={`${estilos.badge} texto-${cor} bg-${cor}-claro`}>
-          {total}
+          {clientesPorStatus.length > 0
+            ? String(clientesPorStatus.length).padStart(2, "0")
+            : 0}
         </span>
       </div>
 
-      <div className={`${estilos.subHeader}`}>
-        <span>Cliente</span>
-        <span>Data de venc.</span>
-        <span>Valor</span>
-      </div>
+      {clientesPorStatus.length > 0 && (
+        <>
+          <div className={`${estilos.subHeader}`}>
+            <span>Cliente</span>
+            <span>ID do Clie.</span>
+            <span>CPF</span>
+          </div>
 
-      <ul className={`${estilos.lista}`}>
-        {lista &&
-          lista.map((item) => (
-            <li key={item.id_cobranca} className={`${estilos.listaItem}`}>
-              <span>{item.cliente}</span>
+          <ul className={`${estilos.lista}`}>
+            {clientesPorStatus.slice(0, 4).map((cliente) => (
+              <li key={cliente.id} className={`${estilos.listaItem}`}>
+                <span>{cliente.nome}</span>
 
-              <span>{item.data}</span>
+                <span>{cliente.id}</span>
 
-              <span>
-                {(item.valor / 100).toLocaleString("pt-BR", {
-                  style: "currency",
-                  currency: "BRL",
-                })}
-              </span>
-            </li>
-          ))}
-      </ul>
+                <span>
+                  {String(cliente.cpf).replace(
+                    /(\d{3})(\d{3})(\d{3})(\d{2})/,
+                    "$1.$2.$3-$4"
+                  )}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
 
-      <div className={`text-center ${estilos.footer}`}>
-        <a href="#">Ver todos</a>
-      </div>
+      {clientesPorStatus.length > 4 && (
+        <div className={`text-center ${estilos.footer}`}>
+          <a href="#">Ver todos</a>
+        </div>
+      )}
     </div>
   );
 };
 
-export default CardCobrancas;
+export default CardClientes;
